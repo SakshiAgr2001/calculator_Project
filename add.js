336f74b30f4dbf6b5f87b24eac9deb2404cd9f93
SakshiAgr2001 <sakshi.agrawal2019@vitstudent.ac.in>
2022-09-28 14:18:01 +0530

    ADD js Added

---
+++ add.js
@@ -0,0 +1,6 @@
+function add() {
+var num1 = document.getElementById("first").value
+var num2 = document.getElementById("sec").value
+var res = num1+num2
+document.getElementById("res").value = res
+
