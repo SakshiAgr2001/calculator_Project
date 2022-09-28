5ae5dfd3cbc17351e9201d43214c053b712e77a4
SakshiAgr2001 <sakshi.agrawal2019@vitstudent.ac.in>
2022-09-28 14:28:05 +0530

    index html file modifief

--- add.js
+++ add.js
@@ -1,6 +1,6 @@
 function add() {
 var num1 = document.getElementById("first").value
 var num2 = document.getElementById("sec").value
-var res = num1+num2
+var res =parseInt( num1) + parseInt(num2)
 document.getElementById("res").value = res
 }
