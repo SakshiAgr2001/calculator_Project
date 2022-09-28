f5d8800c809ff6344a80d18eed1e94a882078639
SakshiAgr2001 <sakshi.agrawal2019@vitstudent.ac.in>
2022-09-28 14:48:15 +0530

    Mul func Added

---
+++ mul.js
@@ -0,0 +1,6 @@
+function mul() {
+var num1 = document.getElementById("first").value
+var num2 = document.getElementById("sec").value
+var res =parseInt( num1) * parseInt(num2)
+document.getElementById("res").value = res
+}
