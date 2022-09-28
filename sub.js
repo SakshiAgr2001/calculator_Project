59d369fb9df61994c5fa6035cb0920a36e52bccd
SakshiAgr2001 <sakshi.agrawal2019@vitstudent.ac.in>
2022-09-28 14:32:27 +0530

    SUB Added Index modified

---
+++ sub.js
@@ -0,0 +1,6 @@
+function sub(){
+var num1 = document.getElementById("first").value
+var num2 = document.getElementById("sec").value
+var res = parseInt(num1) - parseInt(num2)
+ document.getElementById("res").value = res
+}
